6650c9e8bb337999353843281ac59d128bdc05d7
ericy <ericy@infuseai.io>
2020-11-25 11:07:50 +0800

    update tf2 model
    Signed-off-by: ericy <ericy@infuseai.io>

--- tensorflow2/tensorflow2/Model.py
+++ tensorflow2/tensorflow2/Model.py
@@ -2,9 +2,8 @@ import os
 import tensorflow as tf
 
 class Model():
-    def __init__(self):
-        model_url = os.environ["model_url"]
-        self._model = tf.keras.models.load_model(model_url)
+    def __init__(self, model_uri):
+        self._model = tf.keras.models.load_model(model_uri)
         print(self._model.summary())
 
     def predict(self, X, feature_names = None, meta = None):
