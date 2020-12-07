9944a19ece3bf455c76f0f7cbf27559fe7940d37
Jack Pan <jackpan@infuseai.io>
2020-12-07 15:34:59 +0800

    Add pytorch prepackaged server
    Signed-off-by: Jack Pan <jackpan@infuseai.io>

---
+++ pytorch/READEME.md
@@ -0,0 +1,20 @@
+# PyTorch Prepackaged Model Server
+
+## 1. Build the base image
+
+```
+$ make build_rest
+```
+
+## 2. Your model directory structure for Primehub Deploy
+
+`{my-model}`, `{model}.pt`, `{ModelClass}.py` can be replaced to any name. The `ModelClass.py` should contain the class named `PyTorchModel` which defines your model in the class.
+```
+# Directory structure
+.
+-── my-model
+    ├── model.pt
+    └── ModelClass.py
+```
+
+You can check the `gs://primehub-models/pytorch/CIFAR10` as a reference also.
---
+++ pytorch/pytorch/Model.py
@@ -0,0 +1,74 @@
+# Reference: https://github.com/kubeflow/kfserving/blob/master/python/pytorchserver/pytorchserver/model.py
+
+import os, glob
+import numpy as np
+import torch
+import importlib
+import sys
+
+class Model():
+    def __init__(self, model_uri):
+        print(model_uri)
+        print(os.listdir(model_uri))
+
+        self.ready = False
+        self.model = None
+
+        self.device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+        self.model_class_name = 'PyTorchModel'
+        
+        py_files = []
+        for filename in os.listdir(model_uri):
+            if filename.endswith('.py'):
+                py_files.append(filename)
+        if len(py_files) == 1:
+            self.model_class_file = os.path.join(model_uri, py_files[0])
+        elif len(py_files) == 0:
+            raise Exception('Missing PyTorch Model Class File.')
+        else:
+            raise Exception('More than one Python file is detected',
+                            'Only one Python file is allowed within model_uri.')
+
+        model_files = []
+        for filename in os.listdir(model_uri):
+            if filename.endswith('.pt'):
+                model_files.append(filename)
+        if len(model_files) == 1:
+            self.model_file = os.path.join(model_uri, model_files[0])
+        elif len(model_files) == 0:
+            raise Exception('Missing PyTorch Model File.')
+        else:
+            raise Exception('More than one PyTorch Model file is detected',
+                            'Only one PyTorch Model file is allowed within model_uri.')
+
+        # Load the python class into memory
+        sys.path.append(os.path.dirname(self.model_class_file))
+        modulename = os.path.basename(self.model_class_file).split('.')[0].replace('-', '_')
+        model_class = getattr(importlib.import_module(modulename), self.model_class_name)
+
+        # Make sure the model weight is transform with the right device in this machine
+        self.model = model_class().to(self.device)
+        self.model.load_state_dict(torch.load(self.model_file, map_location=self.device))
+        self.model.eval()
+        self.ready = True
+
+    def predict(self, X, feature_names = None, meta = None):
+        with torch.no_grad():
+            try:
+                inputs = torch.from_numpy(X).float().to(self.device)
+            except Exception as e:
+                raise TypeError(
+                    "Failed to initialize Torch Tensor from inputs: %s, %s" % (e, inputs))
+            try:
+                return self.model(inputs).tolist()
+            except Exception as e:
+                raise Exception("Failed to predict %s" % e)
+
+    def tags(self):
+        return {}
+
+    def class_names(self):
+        return []
+
+    def metrics(self):
+        return []
